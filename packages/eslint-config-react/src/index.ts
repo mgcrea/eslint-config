e2aced4962eaaf2fb537fee3ecf9cf370e0f4157
Olivier Louvignes <olivier@mgcrea.io>
2023-01-31 16:02:10 +0100

    initial commit

---
+++ packages/eslint-config-react/src/index.ts
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+    "plugin:testing-library/react",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
+};
