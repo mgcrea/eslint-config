898520a13834da017af4815b9c1252e8f8d97ed8
Olivier Louvignes <olivier@mgcrea.io>
2023-01-31 16:09:53 +0100

    feat(config): eslint-config-node

---
+++ packages/eslint-config-node/src/index.ts
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:testing-library/react",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {},
+};
