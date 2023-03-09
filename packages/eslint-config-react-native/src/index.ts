7061d2d0cf537245726642588e0e694ee7949544
Olivier Louvignes <olivier@mgcrea.io>
2023-03-09 12:22:19 +0100

    feat(config): add eslint-config-react-native

---
+++ packages/eslint-config-react-native/src/index.ts
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+    "plugin:testing-library/react",
+    "@react-native",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { ignoreRestSiblings: true, argsIgnorePattern: "^_" },
+    ],
+  },
+};
