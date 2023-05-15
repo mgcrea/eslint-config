d08018fe64c4b3ea12a6325fa0f1bef5bc7c1f54
Olivier Louvignes <olivier@mgcrea.io>
2023-05-15 09:09:37 +0200

    feat(scripts): tweak update

--- scripts/updatePackages.sh
+++ scripts/updatePackages.sh
@@ -4,4 +4,4 @@ set -e # terminate on first failure
 
 ROOT_DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-${ROOT_DIR}/runPackages.sh "ncu -u"
+${ROOT_DIR}/runPackages.sh "ncu -f @types/node -t minor -u; ncu -x @types/node -u"
