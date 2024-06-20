d57a385ae28e57ac8dbcdf7d319156a8bbaa1ce3
Olivier Louvignes <olivier@mgcrea.io>
2024-06-20 11:40:23 +0200

    wip

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import prettierPlugin from "eslint-plugin-prettier"
+import js from "@eslint/js";
+import globals from "globals";
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  js.configs.recommended,
+  {
+    files: [ "**/*.ts" ],
+    languageOptions: {
+      parser: tsParser,
+      globals: globals.node,
+    },
+    plugins: {
+      // "@typescript-eslint": tsPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      // ...eslintJsPlugin.configs.recommended.rules,
+      ...tsPlugin.configs["recommended"]?.rules,
+      ...prettierPlugin.configs["recommended"]?.rules,
+    }
+  },
+];
